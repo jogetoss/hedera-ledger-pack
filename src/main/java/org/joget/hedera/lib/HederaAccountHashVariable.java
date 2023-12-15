06f71e4b24e4bcbd82db1f8bcd76975c21a446e1
A Ady <adi.arshad200@gmail.com>
2023-12-16 02:11:07 +0800

    Fix and Factorize Hedera Account Hash Variable

--- src/main/java/org/joget/hedera/lib/HederaAccountHashVariable.java
+++ src/main/java/org/joget/hedera/lib/HederaAccountHashVariable.java
@@ -21,7 +21,6 @@ public class HederaAccountHashVariable extends HederaHashVariable {
 
         String accountID = "";
         String tokenID = null;
-        String attributeValue = "";
 
         if (variableKey.contains("[") && variableKey.contains("]")) {
 
@@ -49,163 +48,105 @@ public class HederaAccountHashVariable extends HederaHashVariable {
         }
 
         JSONObject jsonResponse = getMirrorResponse(client, accountID);
-
         String attribute = getAttribute(variableKey);
 
-        if ((attribute != null && !attribute.isEmpty())) {
-            String tempValue = "";
+        if (attribute != null && !attribute.isEmpty()) {
             try {
-                switch (attribute.toLowerCase()) {
-                    case "evmaddress":
-                        attributeValue = jsonResponse.getString("evm_address");
-                        break;
-                    case "accountmemo":
-                        attributeValue = jsonResponse.getString("memo");
-                        break;
-                    case "accountisdeleted":
-                        attributeValue = String.valueOf(jsonResponse.getBoolean("deleted"));
-                        break;
-                    case "receiversignaturerequired":
-                        attributeValue = String.valueOf(jsonResponse.getBoolean("receiver_sig_required"));
-                        break;
-                    case "maxautotokenassociations":
-                        attributeValue = String.valueOf(jsonResponse.getInt("max_automatic_token_associations"));
-                        break;
-                    case "autorenewperiod":
-                        attributeValue = (epochRenewPeriod(String.valueOf(jsonResponse.getInt("auto_renew_period"))));
-                        break;
-                    case "createdtimestamp":
-                        attributeValue = epochTimetoDate(jsonResponse.getString("created_timestamp"));
-                        break;
-                    case "declinereward":
-                        attributeValue = String.valueOf(jsonResponse.getBoolean("decline_reward"));
-                        break;
-                    case "ethereumnonce":
-                        attributeValue = String.valueOf(jsonResponse.getInt("ethereum_nonce"));
-                        break;
-                    case "expirytimestamp":
-                        attributeValue = epochTimetoDate(jsonResponse.getString("expiry_timestamp"));
-                        break;
-                    case "publickeytype":
-                        attributeValue = jsonResponse.getJSONObject("key").getString("_type");
-                        break;
-                    case "publickey":
-                        attributeValue = jsonResponse.getJSONObject("key").getString("key");
-                        break;
-                    case "pendingreward":
-                        attributeValue = String.valueOf(jsonResponse.getInt("pending_reward"));
-                        break;
+                return switch (attribute.toLowerCase()) {
+                    case "evmaddress" -> jsonResponse.getString("evm_address");
 
-                    case "hbarbalance":
-                        String hbarBalance = Hbar
-                                .from(jsonResponse.getJSONObject("balance").getBigDecimal("balance"), HbarUnit.TINYBAR)
+                    case "accountmemo" -> jsonResponse.getString("memo");
+
+                    case "isaccountdeleted" -> String.valueOf(jsonResponse.getBoolean("deleted"));
+
+                    case "receiversignaturerequired" -> String.valueOf(jsonResponse.getBoolean("receiver_sig_required"));
+
+                    case "maxautotokenassociations" -> String.valueOf(jsonResponse.getInt("max_automatic_token_associations"));
+
+                    case "declinereward" -> String.valueOf(jsonResponse.getBoolean("decline_reward"));
+
+                    case "ethereumnonce" -> String.valueOf(jsonResponse.getInt("ethereum_nonce"));
+                    
+                    case "autorenewperiod" -> epochRenewPeriod(String.valueOf(jsonResponse.getInt("auto_renew_period")));
+
+                    case "createdtimestamp" -> epochTimetoDate(jsonResponse.getString("created_timestamp"));
+
+                    case "expirytimestamp" -> epochTimetoDate(jsonResponse.getString("expiry_timestamp"));
+
+                    case "publickeytype" -> jsonResponse.getJSONObject("key").getString("_type");
+
+                    case "publickey" -> jsonResponse.getJSONObject("key").getString("key");
+
+                    case "pendingreward" -> String.valueOf(jsonResponse.getInt("pending_reward"));
+
+                    case "alltokens" -> String.valueOf(jsonResponse.getJSONObject("balance").getJSONArray("tokens"));
+
+                    case "hbarbalance" ->
+                        Hbar.from(jsonResponse.getJSONObject("balance").getBigDecimal("balance"), HbarUnit.TINYBAR)
                                 .toString(HbarUnit.HBAR);
-                        attributeValue = hbarBalance;
-                        break;
 
-                    case "allowances":
-                        if (jsonResponse.has("allowances") && !jsonResponse.isNull("allowances")) {
-                            attributeValue = jsonResponse.get("allowances").toString();
-                        } else
-                            attributeValue = "Does Not Exist";
+                    case "allowances" -> jsonResponse.has("allowances") && !jsonResponse.isNull("allowances")
+                            ? jsonResponse.get("allowances").toString()
+                            : "Does Not Exist";
 
-                        break;
+                    case "alias" -> jsonResponse.has("alias") && !jsonResponse.isNull("alias")
+                            ? jsonResponse.get("alias").toString()
+                            : "Does Not Exist";
 
-                    case "alias":
-                        if (jsonResponse.has("alias") && !jsonResponse.isNull("alias")) {
-                            attributeValue = jsonResponse.get("alias").toString();
-                        } else
-                            attributeValue = "Does Not Exist";
+                    case "rewards" -> jsonResponse.has("rewards") && !jsonResponse.isNull("rewards")
+                            ? jsonResponse.get("rewards").toString()
+                            : "Does Not Exist";
 
-                        break;
+                    case "stakedaccountid" ->
+                        jsonResponse.has("staked_account_id") && !jsonResponse.isNull("staked_account_id")
+                                ? jsonResponse.get("staked_account_id").toString()
+                                : "Does Not Exist";
 
-                    case "rewards":
-                        if (jsonResponse.has("rewards") && !jsonResponse.isNull("rewards")) {
-                            attributeValue = jsonResponse.get("rewards").toString();
-                        } else
-                            attributeValue = "Does Not Exist";
+                    case "stakednodeid" -> jsonResponse.has("staked_node_id") && !jsonResponse.isNull("staked_node_id")
+                            ? jsonResponse.get("staked_node_id").toString()
+                            : "Does Not Exist";
 
-                        break;
+                    case "stakeperiodstart" ->
+                        jsonResponse.has("staked_period_start") && !jsonResponse.isNull("staked_period_start")
+                                ? jsonResponse.get("staked_period_start").toString()
+                                : "Does Not Exist";
 
-                    case "stakedaccountid":
-                        if (jsonResponse.has("staked_account_id") && !jsonResponse.isNull("staked_account_id")) {
-                            attributeValue = jsonResponse.get("staked_account_id").toString();
-                        } else
-                            attributeValue = "Does Not Exist";
-
-                        break;
-
-                    case "stakednodeid":
-                        if (jsonResponse.has("staked_node_id") && !jsonResponse.isNull("staked_node_id")) {
-                            attributeValue = jsonResponse.get("staked_node_id").toString();
-                        } else
-                            attributeValue = "Does Not Exist";
-
-                        break;
-
-                    case "stakeperiodstart":
-                        if (jsonResponse.has("staked_period_start") && !jsonResponse.isNull("staked_period_start")) {
-                            attributeValue = jsonResponse.get("staked_period_start").toString();
-                        } else
-                            attributeValue = "Does Not Exist";
-
-                        break;
-
-                    case "alltokens":
-                        tempValue = jsonResponse.getJSONObject("balance").getJSONArray("tokens").toString();
-                        if (tempValue.equals(JSONObject.NULL) || tempValue == null
-                                || tempValue.isEmpty()) {
-                            attributeValue = "Does Not Exist";
-                        } else
-                            attributeValue = tempValue;
-                        break;
-
-                    default:
-
+                    case "tokenbalance" -> {
                         JSONArray accountTokens = jsonResponse.getJSONObject("balance").getJSONArray("tokens");
-                        String tokenBalance = "";
 
                         for (int i = 0; i < accountTokens.length(); i++) {
                             JSONObject token = accountTokens.getJSONObject(i);
                             String tokenId = token.get("token_id").toString();
 
                             if (tokenId.equals(tokenID)) {
-                                tokenBalance = token.get("balance").toString();
-
-                                attributeValue = tokenBalance;
-                                break;
-                            } else
-                                attributeValue = "Does Not Exist";
-
+                                yield token.get("balance").toString();
+                            }
                         }
-
-                        break;
-                }
+                        yield "Does Not Exist";
+                    }
+                    default -> null;
+                };
             } catch (Exception e) {
                 LogUtil.error(HederaAccountHashVariable.class.getName(), e,
                         "Error retrieving user attribute " + attribute);
             }
-
         }
 
-        return attributeValue;
+        return null;
 
     }
 
     private String getAttribute(String variableKey) {
 
         // Detect the attribute in variableKey
-        String attribute ="";
         for (String v : availableSyntax()) {
             v = v.replaceAll("hedera.account", "");
-            LogUtil.debug(HederaHashVariable.class.getName(), "Modified v: " + v);
 
             if (variableKey != null && variableKey.equalsIgnoreCase(v)) {
-                attribute = v.substring(1);
-                LogUtil.debug(HederaHashVariable.class.getName(), "attribute: " + attribute);
+                return v.substring(1);
             }
         }
-        return attribute;
+        return null;
     }
 
     private JSONObject getMirrorResponse(Client client, String accountID) {
@@ -220,7 +161,6 @@ public class HederaAccountHashVariable extends HederaHashVariable {
     }
 
     public String epochRenewPeriod(String epochTime) {
-        String date = "";
         try {
             if (!epochTime.contains(".")) {
                 long epoch = Integer.valueOf(epochTime);
@@ -229,7 +169,7 @@ public class HederaAccountHashVariable extends HederaHashVariable {
 
                 long days = duration.toDays();
 
-                date = days + " Days";
+                return days + " Days";
             }
 
         } catch (Exception e) {
@@ -237,18 +177,16 @@ public class HederaAccountHashVariable extends HederaHashVariable {
                     "Epoch Time Error " + epochTime);
         }
 
-        return date;
+        return null;
     }
 
     public String epochTimetoDate(String epochTime) {
-
-        String date = " ";
 
         try {
             if (epochTime.contains(".")) {
                 double epoch = Double.valueOf(epochTime);
 
-                date = new java.text.SimpleDateFormat("dd-MM-yyyy hh:mm:ss a z")
+                return new java.text.SimpleDateFormat("dd-MM-yyyy hh:mm:ss a z")
                         .format(new java.util.Date((long) epoch * 1000)).toUpperCase();
             }
 
@@ -257,7 +195,7 @@ public class HederaAccountHashVariable extends HederaHashVariable {
                     "Epoch Time Error " + epochTime);
         }
 
-        return date;
+        return null;
 
     }
 
@@ -293,7 +231,7 @@ public class HederaAccountHashVariable extends HederaHashVariable {
         syntax.add("hedera.account.tokenBalance");
         syntax.add("hedera.account.hbarBalance");
         syntax.add("hedera.account.accountMemo");
-        syntax.add("hedera.account.accountIsDeleted");
+        syntax.add("hedera.account.isAccountDeleted");
         syntax.add("hedera.account.receiverSignatureRequired");
         syntax.add("hedera.account.evmAddress");
         syntax.add("hedera.account.maxAutoTokenAssociations");
